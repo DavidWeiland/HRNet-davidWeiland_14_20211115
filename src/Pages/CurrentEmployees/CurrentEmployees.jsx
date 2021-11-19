52a10617bd19c08e8271ccba41fb69f71a0bd32d
David Weiland <weiland.david@icloud.com>
2021-11-19 14:21:53 +0100

    pages of project

--- src/Pages/CurrentEmployees/CurrentEmployees.jsx
+++ src/Pages/CurrentEmployees/CurrentEmployees.jsx
@@ -1,14 +1,15 @@
 import '../../Styles/App.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Table from '../../Components/Table';
 
 function CurrentEmployees() {
+  const employees = JSON.parse(localStorage.getItem('employees'));
+  
   return (
     <div id="employee-div" class="container">
       <h1>Current Employees</h1>
 
-      ici travailler table : plugin jQuery
-
-      <table id="employee-table" class="display"></table>
+      <Table dataSource={employees} />
 
       <Link to="/">Home</Link>
     </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,68 +1,145 @@
 import '../../Styles/App.css';
-import {Link} from 'react-router-dom'
+import { useState, useRef } from 'react';
+import { Link } from 'react-router-dom'
+import Datetimepicker from '../../Components/DateTimePicker';
+import { states } from '../../Data/States'
+import SelectMenu from '../../Components/SelectMenu';
+import SelectOptions from '../../Components/SelectOptions';
+import $ from 'jquery';
+import 'jquery-modal'
+import 'jquery-modal/jquery.modal.css'
 
-function Home() {
+export default function Home() {
+  const [firstName, setFirstname]= useState('')
+  const [lastName, setLastname]= useState('')
+  const [dateOfBirth, setBirthdate]= useState('')
+  const [startDate, setStartdate]= useState('')
+  const [street, setStreet]= useState('')
+  const [city, setCity]= useState('')
+  const [state, setState]= useState('AL')
+  const [zipCode, setZipcode ]= useState('')
+  const [department, setDepartment] = useState('Sales')
+
+  const modalRef = useRef()
+
+  const handleChange = (e) => {
+    const value = e.target.value
+    switch (e.target.id) {
+      case "first-name":
+        setFirstname(value)
+        break
+      case "last-name":
+        setLastname(value)
+        break
+      case "street":
+        setStreet(value)
+        break
+      case "city":
+        setCity(value)
+        break
+      case "zip-code":
+        setZipcode(value)
+        break
+      default:
+    }
+  }
+
   const saveEmployee = () => {
-    
+    const employees = JSON.parse(localStorage.getItem('employees')) || []
+    const employee = {
+        firstName: firstName,
+        lastName: lastName,
+        dateOfBirth: dateOfBirth,
+        startDate: startDate,
+        street: street,
+        city: city,
+        state: state,
+        zipCode: zipCode,
+        department: department
+    }
+    employees.push(employee);
+    localStorage.setItem('employees', JSON.stringify(employees))
+
+    $(modalRef.current).modal()
+
+    setFirstname('')
+    setLastname('')
+    setBirthdate('')
+    setStartdate('')
+    setStreet('')
+    setCity('')
+    setState('AL')
+    setZipcode('')
+    setDepartment('Sales')
+
   }
 
   return (
     <div>
-      <div class="title">
-          <h1>HRnet</h1>
+      <div className="title">
+        <h1>HRnet</h1>
       </div>
-      <div class="container">
+      
+      <div className="container">
         <Link to="/currentEmployees">View Current Employees</Link>
         <h2>Create Employee</h2>
         <form action="#" id="create-employee">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <input type="text" id="first-name" value={firstName} onChange={handleChange} />
 
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <input type="text" id="last-name" value={lastName} onChange={handleChange}/>
 
           <label htmlFor="date-of-birth">Date of Birth</label>
-          ici travailler DateTimePicker : plugin jQuery
-          <input id="date-of-birth" type="text" />
+          <Datetimepicker id="date-of-birth" value={dateOfBirth} onChange={setBirthdate}/>
 
           <label htmlFor="start-date">Start Date</label>
-          ici travailler DateTimePicker : plugin jQuery
-          <input id="start-date" type="text" />
+          <Datetimepicker id="start-date" value={startDate} onChange={setStartdate}/>
 
-          <fieldset class="address">
+          <fieldset className="address">
             <legend>Address</legend>
 
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
+            <input id="street" type="text" value={street} onChange={handleChange}/>
 
             <label htmlFor="city">City</label>
-            <input id="city" type="text" />
+            <input id="city" type="text" value={city} onChange={handleChange}/>
 
             <label htmlFor="state">State</label>
-            ici travailler menu déroulant : plugin jQuery
-            <select name="state" id="state"></select>
+            <SelectMenu name="state" id="state" onChange={setState}>
+              {states.map(({ index, name, abbreviation }) => (
+                <SelectOptions
+                  key={`${abbreviation}-${index}`}
+                  value={abbreviation}
+                  text={name}
+                />
+              ))}
+            </SelectMenu>
 
             <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+            <input id="zip-code" type="number" value={zipCode} onChange={handleChange}/>
           </fieldset>
 
           <label htmlFor="department">Department</label>
-          ici travailler menu déroulant : plugin jQuery
-          <select name="department" id="department">
+          <SelectMenu name="department" id="department" onChange={setDepartment}>
             <option>Sales</option>
             <option>Marketing</option>
             <option>Engineering</option>
             <option>Human Resources</option>
             <option>Legal</option>
-          </select>
+          </SelectMenu>
         </form>
 
-        <button onclick={saveEmployee()}>Save</button>
+        <button onClick={saveEmployee}>
+          Save
+        </button>
       </div>
-      ici travailler modal : plugin jQuery
-      <div id="confirmation" class="modal">Employee Created!</div>
+      <div
+        ref={modalRef}
+        id="confirmation"
+        className="modal">
+        Employee Created!
+      </div>
     </div>
   );
 }
-
-export default Home;
