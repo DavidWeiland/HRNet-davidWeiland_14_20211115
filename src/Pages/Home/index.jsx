2153a06d2bf5af0ee17f2ea846ef2277d40c6a79
David Weiland <weiland.david@icloud.com>
2022-03-29 14:42:29 +0200

    add lazy to perform app

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,17 +1,14 @@
 import '../../Utils/Styles/App.css';
-import React, { useState, Suspense } from 'react'
+import { useState, Suspense, lazy } from 'react'
 import { Link } from 'react-router-dom'
 import { useStore } from 'react-redux'
 import { addEmployee } from '../../Features/employees';
-import { states } from '../../Data/States'
-import Datetimepicker from '../../Components/DateTimePicker';
 import SelectMenu from '../../Components/SelectMenu';
-import SelectOptions from '../../Components/SelectOptions';
 import InputComponent from '../../Components/InputComponent';
+import { formatDate } from '../../Utils/FormatDateFunction';
 
 // personal npm module:
-//import Modal from 'dw-modal'
-const Modal = React.lazy(() => import('dw-modal'))
+const Modal = lazy(() => import('dw-modal'))
 
 export default function Home() {
   const store = useStore()
@@ -20,31 +17,42 @@ export default function Home() {
   //localStorage.removeItem('employees')
 
   // Controls states of inputs
-  const [firstName, setFirstname]= useState('')
-  const [lastName, setLastname]= useState('')
-  const [dateOfBirth, setBirthdate]= useState('')
-  const [startDate, setStartdate]= useState('')
-  const [street, setStreet]= useState('')
-  const [city, setCity]= useState('')
-  const [state, setState]= useState('AL')
-  const [zipCode, setZipcode ]= useState('')
+  const [firstName, setFirstname] = useState('')
+  const [lastName, setLastname] = useState('')
+  const [dateOfBirth, setBirthdate] = useState('')
+  const [startDate, setStartdate] = useState('')
+  const [street, setStreet] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('AL')
+  const [zipCode, setZipcode] = useState('')
   const [department, setDepartment] = useState('Sales')
-  
+
   // Controls the display of the modal
   const [modal, setModal] = useState(false)
 
   // Function to save a new employee
   const saveEmployee = () => {
-
     // Prepares data to be added to the store
-    const employee = { firstName, lastName, dateOfBirth, startDate, street, city, state, zipCode, department }
+    const employee = {
+      firstName,
+      lastName,
+      dateOfBirth: formatDate(dateOfBirth),
+      startDate: formatDate(startDate),
+      street,
+      city,
+      state,
+      zipCode,
+      department,
+    }
+    
+    console.log(employee.dateOfBirth)
     addEmployee(store, employee)
 
     // This following part is only necessary in the case of a localStorage
-    const employees = JSON.parse(localStorage.getItem('employees')) || []
+    /* const employees = JSON.parse(localStorage.getItem('employees')) || []
     employees.push(employee)
-    localStorage.setItem('employees', JSON.stringify(employees))
-
+    localStorage.setItem('employees', JSON.stringify(employees)) */
+ 
     // Opens the modal
     setModal(!modal)
 
@@ -64,7 +72,7 @@ export default function Home() {
   const closeModal = () => {
     setModal(!modal)
   }
-
+  
   return (
     <div>
       <div className="title">
@@ -90,17 +98,19 @@ export default function Home() {
             onChange={(e) => setLastname(e.target.value)}
           />
 
-          <label htmlFor="date-of-birth">Date of Birth</label>
-          <Datetimepicker
+          <InputComponent
+            type="date"
             id="date-of-birth"
+            label="Date of Birth"
             value={dateOfBirth}
-            onChange={setBirthdate}
+            onChange={(e) => setBirthdate(e.target.value)}
           />
-          <label htmlFor="start-date">Start Date</label>
-          <Datetimepicker
+          <InputComponent
+            type="date"
             id="start-date"
+            label="Start Date"
             value={startDate}
-            onChange={setStartdate}
+            onChange={(e) => setStartdate(e.target.value)}
           />
 
           <fieldset className="address">
@@ -108,7 +118,7 @@ export default function Home() {
             <InputComponent
               type="text"
               id="street"
-              label="street"
+              label="Street"
               value={street}
               onChange={(e) => setStreet(e.target.value)}
             />
@@ -120,16 +130,12 @@ export default function Home() {
               onChange={(e) => setCity(e.target.value)}
             />
 
-            <label htmlFor="state">State</label>
-            <SelectMenu name="state" id="state" onChange={setState}>
-              {states.map(({ index, name, abbreviation }) => (
-                <SelectOptions
-                  key={`${abbreviation}-${index}`}
-                  value={abbreviation}
-                  text={name}
-                />
-              ))}
-            </SelectMenu>
+            <SelectMenu
+              name="state"
+              id="state"
+              label="State"
+              onChange={(e) => setState(e.target.value)}
+            />
 
             <InputComponent
               type="number"
@@ -140,25 +146,19 @@ export default function Home() {
             />
           </fieldset>
 
-          <label htmlFor="department">Department</label>
           <SelectMenu
             name="department"
             id="department"
-            onChange={setDepartment}
-          >
-            <option>Sales</option>
-            <option>Marketing</option>
-            <option>Engineering</option>
-            <option>Human Resources</option>
-            <option>Legal</option>
-          </SelectMenu>
-
+            label="Department"
+            onChange={(e) => setDepartment(e.target.value)}
+          />
         </form>
 
         <button onClick={saveEmployee}>Save</button>
       </div>
+
       <Suspense fallback={<div></div>}>
-          <Modal state={modal} config={{}} close={closeModal}>
+        <Modal state={modal} config={{}} close={closeModal}>
           <p>Employee Created!</p>
         </Modal>
       </Suspense>
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import CurrentEmployees from './Pages/CurrentEmployees'
-import Error from './Pages/Error'
+//import Home from './Pages/Home';
+//import CurrentEmployees from './Pages/CurrentEmployees'
+//import Error from './Pages/Error'
 import { Provider } from 'react-redux'
 import store from './Utils/store'
+
+const Home = lazy(() => import('./Pages/Home'))
+const CurrentEmployees = lazy(() => import('./Pages/CurrentEmployees'))
+const Error = lazy(()=>import('./Pages/Error'))
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <Router>
+        <Suspense fallback={<div>Chargement...</div>}>
         <Switch>
           <Route exact path="/">
             <Home />
@@ -22,6 +27,8 @@ ReactDOM.render(
             <Error />
           </Route>
         </Switch>
+        </Suspense>
+
       </Router>
     </React.StrictMode>
   </Provider>,
