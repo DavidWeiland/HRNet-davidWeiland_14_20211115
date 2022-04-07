4397271f5e531b740f61d0cfe5cac7c93e37ad0a
David Weiland <weiland.david@icloud.com>
2022-04-07 18:26:28 +0200

    move DW-Table component

--- src/Pages/CurrentEmployees/index.jsx
+++ src/Pages/CurrentEmployees/index.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectEmployees } from '../../Utils/selectors'
 
+//import personal component 
 import { Table } from '@david_weiland/dw-table'
 
 function CurrentEmployees() {
   // Link to the store
   const employees = useSelector(selectEmployees)
-  const employeesList = employees.data
+  const employeesList = employees.data ?? {}
 
   return (
     <div>
@@ -18,35 +19,19 @@ function CurrentEmployees() {
 
       <div id="employee-div" className="container">
         <Table
-          data={employeesList ?? {}}
+          data={employeesList}
           model={[
-            {
-              title: 'First Name',
-              value: 'firstName',
-              type: 'string',
-              flex: 2,
-            },
+            { title: 'First Name', value: 'firstName', type: 'string', flex: 2 },
             { title: 'Last Name', value: 'lastName', type: 'string', flex: 3 },
             { title: 'Start Date', value: 'startDate', type: 'date', flex: 2 },
-            {
-              title: 'Department',
-              value: 'department',
-              type: 'string',
-              flex: 3,
-            },
-            {
-              title: 'Date of Birth',
-              value: 'dateOfBirth',
-              type: 'date',
-              flex: 2,
-            },
+            { title: 'Department', value: 'department', type: 'string', flex: 3 },
+            { title: 'Date of Birth', value: 'dateOfBirth', type: 'date', flex: 2},
             { title: 'Street', value: 'street', type: 'string', flex: 3 },
             { title: 'City', value: 'city', type: 'string', flex: 3 },
             { title: 'State', value: 'state', type: 'string', flex: 1 },
             { title: 'Zip Code', value: 'zipCode', type: 'number', flex: 2 },
           ]}
         />
-        {/* <Table dataSource={employeesList ?? {}} /> */}
       </div>
 
       <div className="subtitle_container">
