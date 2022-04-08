d2fde811c295e5d05c5483c4735c2e99b93a8ef9
David Weiland <weiland.david@icloud.com>
2022-04-08 11:18:06 +0200

    move to dw-table component

--- src/Utils/FormatDateFunction/index.jsx
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateStr) => {
-    if (dateStr) {
-      const date = new Date(dateStr)
-      const day = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
-      const month = new Intl.DateTimeFormat('fr', { month: '2-digit' }).format(date)
-      const year = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
-      return `${month}/${day}/${year}`
-    }
-}
